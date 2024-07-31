c464e574af57706947dd5b2b0afa5b732bc34812
CygnusST3RN <rahul880999@gmail.com>
2024-07-31 14:33:06 +0530

    Added Service in frontend

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,3 +1,42 @@
+import { useAuth } from "../store/auth"
+
 export const Services = () => {
-    return <h1>Hello This is the Services page</h1>
+
+    const {services} = useAuth();
+
+    return <>
+        <section className="section-services">
+            <div className="container">
+                <h1 className="main-heading">
+                    Services
+                </h1>
+            </div>
+
+            <div className="container grid grid-three-cols">
+                { 
+                services.map((curElem, index)=>{
+                    
+                    const {description, price, provider, service} = curElem;
+                    return (
+                <div className="card" key={index}>
+                    <div className="card-image">
+                        <img src="/images/design.png" alt="service1 image" width={400} />
+                    </div>
+                    <div className="card-details">
+                        <div className="grid grid-two-cols">
+                            <p>{provider}</p>
+                            <p>{price}</p>
+                        </div>
+                        <p>{service}</p>
+                        <p>{description}</p>
+                    </div>
+                </div>
+                );
+                })
+                }
+
+            </div>
+        </section>
+    
+    </>
 }
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -6,6 +6,8 @@ export const AuthProvider = ({children}) => {
 
         const [token, setToken] = useState(localStorage.getItem('token'));
         const [user, setUser] = useState("");
+
+        const [services, setServices] = useState([]);
         const storeTokenInLS = (serverToken) => {
             setToken("token");
             return localStorage.setItem('token', serverToken);
@@ -41,12 +43,31 @@ export const AuthProvider = ({children}) => {
         
             }
         }
+
+        //to render services in the Service frontend page
+        const getServices = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/api/data/service', {
+                    method: "GET",
+                });
+
+                if(response.ok){
+                    const serviceData = await response.json();
+                    console.log(serviceData.msg);
+                    setServices(serviceData.msg);
+                }
+
+            } catch (error) {
+                console.log("service frontend error: ",error);
+            }
+        }
         useEffect( () => {
+            getServices();
             userAuthentication();
         }, []);
 
         return (
-        <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, logoutUser, user}}>
+        <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, logoutUser, user, services}}>
             {children}
         </AuthContext.Provider>
         );
